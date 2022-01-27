6c1e5f9c6c77e3409ddcb771e0c218e9a31d24a8
D0rkKnight <shouhanzen@gmail.com>
2022-01-26 17:17:33 -0800

    Revert "java -cp ".;libs\*" org.junit.runner.JUnitCore MarkdownParseTest"
    This reverts commit 4ccc4ea6d0aee7a5f3182a1a876d29ec24e1a565.

--- MarkdownParseTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-import org.junit.*; //importing junit test
- 
-public class MarkdownParseTest { //defining a class 
-    @Test //marks the start of test
-    public void addition() { // method definition
-        assertEquals(2, 1 + 1); //testing 1 + 1 = 2 or not.
-    }
-}
--- run.bat
+++
@@ -1,5 +0,0 @@
-ECHO Running test
-
-javac MarkdownParse.java
-javac -cp ".;libs\*" MarkdownParseTest.java
-java -cp ".;libs\*" org.junit.runner.JUnitCore MarkdownParseTest
